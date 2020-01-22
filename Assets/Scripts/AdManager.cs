fec67e936bc18884d40331c3fce4c544032b7f34
UnityProjectTjl <60152780+UnityProjectTjl@users.noreply.github.com>
2020-01-22 17:37:44 +0100

    Initial Commit

---
+++ Assets/Scripts/AdManager.cs
@@ -0,0 +1,52 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdManager : MonoBehaviour
+{
+    private InterstitialAd interstitial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize(initStatus => { });
+
+        this.RequestInterstitial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();
+    }
+
+    private void RequestInterstitial()
+    {
+        #if UNITY_ANDROID
+                string adUnitId = "ca-app-pub-3940256099942544/1033173712";
+        #elif UNITY_IPHONE
+                string adUnitId = "ca-app-pub-3940256099942544/4411468910";
+        #else
+                string adUnitId = "unexpected_platform";
+        #endif
+
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(adUnitId);
+
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the interstitial with the request.
+        this.interstitial.LoadAd(request);
+
+        GameOver();
+    }
+
+    private void GameOver()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
+}
---
+++ Assets/Scripts/Controlls.cs
@@ -0,0 +1,116 @@
+﻿using UnityEngine;
+
+public class Controlls : MonoBehaviour
+{
+    Vector2 startPos, endPos, distance;
+    public float speed, jumpspeed;
+    public float toleranz;
+    bool directionChosen;
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        //Richtung d. Spielers wechseln
+
+        //Wenn touched:
+        if (directionChosen)
+        {
+            //Start < Ende --> Nach rechts bewegen/swipe nach rechts
+
+            if (startPos.x < endPos.x)
+            {
+                if (distance.x >= toleranz)
+                {
+                    //Spieler nach rechts bewegen
+                    rb2d.velocity = new Vector2(speed, 0);
+                }
+
+
+            }
+            //Start > Ende --> Nach links bewegen/Swipe nach links
+            else
+                    if (startPos.x > endPos.x)
+            {
+                if (distance.x <= -toleranz)
+                {
+                    //nach links bewegen
+                    rb2d.velocity = new Vector2(-speed, 0);
+                }
+
+            }
+
+
+            if (startPos.y < endPos.y)
+            {
+                if (distance.y >= toleranz)
+                {
+                    //Nach oben bewegen
+                    rb2d.AddForce(new Vector2(0, jumpspeed), ForceMode2D.Impulse);
+                }
+
+            }
+            else
+                if (startPos.y > endPos.y)
+            {
+                if (distance.y<= -toleranz)
+                {
+                    //Nach unten bewegen
+                    rb2d.AddForce(new Vector2(0, -jumpspeed), ForceMode2D.Impulse);
+                }
+
+            }
+            directionChosen = false;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Touches: " + Input.touchCount);
+
+        if (Input.touchCount == 3)
+        {
+            print("Drei Finger touch");
+        }
+
+        //Touch Display wird berührt
+        if (Input.touchCount > 0)
+        {
+            //Ja=> Start der Touch Phase
+            Touch touch = Input.GetTouch(0);
+
+            switch (touch.phase)
+            {
+                // - Start (Spieler legt den Finger auf Display) == Koordinate
+                case TouchPhase.Began:
+                    //Position d. Fingers abspeichern
+                    startPos = touch.position;
+                    directionChosen = false;
+
+                    break;
+
+                // - Ende (Spieler entfernt Finger von Display) == Koordinate
+
+                case TouchPhase.Ended:
+                    endPos = touch.position;
+                    distance = endPos - startPos;
+                    Debug.Log("Start: " + startPos + "Ende: " + endPos + " Distanz: " + distance);
+                    directionChosen = true;
+                    break;
+
+            }
+
+
+
+
+        }
+
+    }
+}
