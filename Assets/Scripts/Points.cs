5a36ed58dd2d332782563e3f86d0b48cac8b443f
UnityProjectTjl <60152780+UnityProjectTjl@users.noreply.github.com>
2020-01-24 18:11:37 +0100

    Github-Uploadtest
    Kleiner Korb hinzugefügt,
    ball verschwindet komischerweise,
    sobald man drinne is

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Points : MonoBehaviour
+{
+    public Text points;
+    private int score = 0;
+    public GameObject checkpoint, player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            score++;
+            points.text = "Points: " + score;
+            player.transform.position = checkpoint.transform.position;
+        }
+    }
+}
