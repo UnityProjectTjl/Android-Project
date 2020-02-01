28b6850ba666f732f39f6bbcb6ef20571ca5d1a0
TIMM-Unity <timm.ditz@gmail.com>
2020-02-01 13:03:59 +0100

    Mehrere Änderungen
    --> Skalierendes Panel hinzugefügt
    --> Hauptmenü hinzugefügt (Scenes/Screens/MainMenu)
          --> MenuManager und MenuManager.cs hinzugefügt
    	 (Kann erweitert werden)
    --> Vorläufiges UI des Shops erstellt (Scenes/Screens/Shop)
          --> ImageSwitcher.cs und Buy.cs hinzugefügt -> noch nicht fertig,
                  brauche noch Sprites! (Scripts/Shop/)
          --> Sprites bitte in Sprites/Skins ablegen

---
+++ Assets/Scripts/Shop/ImageSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageSwitcher : MonoBehaviour
+{
+
+    public Material[] skins;
+    int ImageIndex = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowNextImage()
+    {
+
+    }
+    public void SchowLastImage()
+    {
+        if (ImageIndex >= 1)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
