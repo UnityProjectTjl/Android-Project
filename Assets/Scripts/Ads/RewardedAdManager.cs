0004636f3482ca097b6590e717e6998707a003d2
JonasH2002 <jonas.hensel@online.de>
2020-02-14 19:11:36 +0100

    IAP durch Rewarded Ads ersetzt!

---
+++ Assets/Scripts/Ads/RewardedAdManager.cs
@@ -0,0 +1,37 @@
+﻿using GoogleMobileAds.Api;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewardedAdManager : MonoBehaviour
+{
+    private RewardedAd rewardedAd;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rewardedAd = new RewardedAd("ca-app-pub-3940256099942544/5224354917");
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+        // Load the rewarded ad with the request.
+        this.rewardedAd.LoadAd(request);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void UserChoseToWatchAd()
+    {
+        if (this.rewardedAd.IsLoaded())
+        {
+            this.rewardedAd.Show();
+        }
+    }
+
+}
