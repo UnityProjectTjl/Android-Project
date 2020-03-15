e0fdf13fd1eef3974af5b1e8602cd2881ab50208
JonasH2002 <jonas.hensel@online.de>
2020-03-15 19:29:05 +0100

    Location Permission wird abgefragt--> Wichtig bei Build Settings das Projekt erst exportieren und dann mit Android Studio apk builden.

---
+++ Assets/Scripts/RequestPermission.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Android;
+
+public class RequestPermission : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Permission.HasUserAuthorizedPermission(Permission.CoarseLocation))
+        {
+            // The user authorized use of the Location.
+        }
+        else
+        {
+            // We do not have permission to use the Location.
+            // Ask for permission or proceed without the functionality enabled.
+            Permission.RequestUserPermission(Permission.CoarseLocation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
